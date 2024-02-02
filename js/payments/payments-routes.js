65ebf62232f2faa11290fcc08a8661804b650e34
James Barnes <jamesbarnes20082000@gmail.com>
2024-02-02 14:09:50 +0000

    restructure files

---
+++ js/payments/payments-routes.js
@@ -0,0 +1,1 @@
+const payments = require('./payments.js');
--- js/server.js
+++ js/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const middleware = require('./middleware.js');
-const users = require('./users.js');
-const payments = require('./payments.js');
+const usersRoutes = require('./users/users-routes.js');
+const paymentsRoutes = require('./payments/payments-routes.js');
 
 //configure express to listen on port
 const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
+app.use('/users', usersRoutes);
+app.use('/payments', paymentsRoutes);
 app.listen(PORT, () => {
     console.log("Server Listening on port:", port);
   });
